d9bbd926ba60320430bd355695155cc04b7bb99a
Ayush Singh <Ayushsingh12525@gmail.com>
2020-05-19 14:35:38 +0530

    19-05-2020

--- FaceRecog_VGG16.py
+++ FaceRecog_VGG16.py
@@ -68,6 +68,17 @@ vgg16 = VGG16(weights = 'imagenet',
 for layer in vgg16.layers:
     layer.trainable = False
 
+#this Function will be used for Fine tunning
+def addTopModel(bottom_model, num_classes, D=256):
+    """creates the top or head of the model that will be
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dropout(0.3)(top_model)
+    top_model = Dense(num_classes, activation = "softmax")(top_model)
+    return top_model
+
 # Number of classes in the Flowers-17 dataset
 num_classes = 11
 
