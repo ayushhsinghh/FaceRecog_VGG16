849716188728db4843aa82703072dfea3805e38c
Ayush Singh <Ayushsingh12525@gmail.com>
2020-05-19 14:24:14 +0530

    19-05-2020

---
+++ FaceRecog_VGG16.py
@@ -0,0 +1,122 @@
+from keras.applications import VGG16
+
+# Setting the input size now to 64 x 64 pixel
+#Training using 64 x 64 image size is MUCH faster!
+img_rows = 64
+img_cols = 64
+
+# Re-loads the VGG16 model without the top or FC layers
+vgg16 = VGG16(weights = 'imagenet',
+                 include_top = False,
+                 input_shape = (img_rows, img_cols, 3))
+
+# Here we freeze the last 4 layers
+# Layers are set to trainable as True by default
+for layer in vgg16.layers:
+    layer.trainable = False
+
+# Use this to print our layers
+#for (i,layer) in enumerate(vgg16.layers):
+#    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+from keras.applications import VGG16
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+
+# Used 11 Directory of Different Persons
+train_data_dir = 'Train/'
+validation_data_dir = 'Test/'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Re-loads the VGG16 model without the top or FC layers
+vgg16 = VGG16(weights = 'imagenet',
+                 include_top = False,
+                 input_shape = (img_rows, img_cols, 3))
+
+# Freeze layers
+for layer in vgg16.layers:
+    layer.trainable = False
+
+# Number of classes in the Flowers-17 dataset
+num_classes = 11
+
+FC_Head = addTopModel(vgg16, num_classes)
+
+model = Model(inputs=vgg16.input, outputs=FC_Head)
+
+#let Train The Model
+
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+checkpoint = ModelCheckpoint("Face_recog.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss',
+                          min_delta = 0,
+                          patience = 5,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss',
+                              factor = 0.2,
+                              patience = 3,
+                              verbose = 1,
+                              min_delta = 0.00001)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint, reduce_lr]
+
+# Note we use a very small learning rate
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.0001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 25
+batch_size = 32
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+model.save("Face_recog.h5")
